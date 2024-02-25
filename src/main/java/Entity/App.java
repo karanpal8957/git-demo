4da9d60e637d42aa28e6744423146c458adceb84
karanpal8957 <karanpal8958@gmail.com>
2024-02-25 21:00:41 +0530

    added constant class and change in App.java

---
+++ src/main/java/Entity/App.java
@@ -0,0 +1,17 @@
+package Entity;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.gitDemo.Constant;
+
+public class App {
+
+    public void addData(String data){
+        if(data.equals(Constant.DATA_TYPE)){
+            System.out.println("Welcome");
+        }
+
+    }
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+}
