bebaffe3ffefd7d0f2a80d1fd58851888747f7c9
karanpal8957 <karanpal8958@gmail.com>
2024-02-25 19:31:31 +0530

    added git demo project to local repo

---
+++ src/main/java/Entity/Karan.java
@@ -0,0 +1,10 @@
+package Entity;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+public class Karan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
